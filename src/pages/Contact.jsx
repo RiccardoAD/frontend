537e1f9c90e58a05b80f3ece4be611c3b9372725
RiccardoAD <rick_95@libero.it>
2024-12-11 19:01:33 +0100

    add responsive profile

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,24 @@
-
+import { assets } from '../assets'
 
 function Contact() {
   return (
     <div>
-      
+      <div className='text-center text-2xl pt-10 text-gray-500'>
+        <p>CONTACT <span className='text-gray-700 font-semibold'>US</span></p>
+      </div>
+
+      <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28 text-sm'>
+        <img className='w-full md:max-w-[360px]' src={assets.contact_image} alt="" />
+        <div className='flex flex-col justify-center items-start gap-6'>
+          <p className=' font-semibold text-lg text-gray-600'>OUR OFFICE</p>
+          <p className=' text-gray-500'>54709 Willms hendimburg <br /> Suite 350, Lione, eu</p>
+          <p className=' text-gray-500'>Tel: (415) 555-0132 <br /> Email: greatstackdev@gmail.com</p>
+          <p className=' font-semibold text-lg text-gray-600'>CAREERS AT PRESCRIPTO</p>
+          <p className=' text-gray-500'>Learn more about our teams and job openings.</p>
+          <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
+        </div>
+      </div>
+
     </div>
   )
 }
--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -23,7 +23,7 @@ return (
 
 
   <div className='max-w-lg flex flex-col gap-2 text-sm'>
-  <img className='w-36 rounded' src={userData.image} alt="" />
+  <img className='w-36 rounded' src={userData.img} alt="" />
 
   {isEdit
       ? <input className='bg-gray-50 text-3xl font-medium max-w-60 mt-4' type="text" onChange={(e) => setUserData(prev => ({ ...prev, name: e.target.value }))} value={userData.name} />
